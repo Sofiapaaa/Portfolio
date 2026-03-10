22265b8bcdd60a4602a6fb28d7e74bc5d96457da
Sofiapaaa <SPassaler@students.ajc-bk.de>
2026-03-10 08:30:55 +0000

    Everything

---
+++ Card.jsx
@@ -0,0 +1,14 @@
+
+function Card(){
+    <div className="card">
+        <img></img>
+        <h3>Sofia Passaler</h3>
+        <ul>
+            <li>2nd year Apprentice</li>
+            <li>Developer</li>            
+        </ul>     
+    </div>
+
+}
+
+export default Card
